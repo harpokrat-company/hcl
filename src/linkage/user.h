8c6bbac10fe92022737f184ef668529880b66159
Fantin Bibas <fantin.bibas@epitech.eu>
2020-01-14 13:24:28 +0000

    Add user.h prototypes

--- src/linkage/user.h
+++ src/linkage/user.h
@@ -9,7 +9,16 @@
 #include "../services/Harpokrat/User.h"
 
 extern "C" {
-
+HCL::User *EXPORT_FUNCTION CreateUser(const char *, const char *, const char *, const char *);
+const char *EXPORT_FUNCTION GetEmailFromUser(HCL::User *);
+void EXPORT_FUNCTION UpdateUserEmail(HCL::User *, const char *);
+const char *EXPORT_FUNCTION GetPasswordFromUser(HCL::User *);
+void EXPORT_FUNCTION UpdateUserPassword(HCL::User *, const char *);
+const char *EXPORT_FUNCTION GetFirstNameFromUser(HCL::User *);
+void EXPORT_FUNCTION UpdateUserFirstName(HCL::User *, const char *);
+const char *EXPORT_FUNCTION GetLastNameFromUser(HCL::User *);
+void EXPORT_FUNCTION UpdateUserLastName(HCL::User *, const char *);
+void EXPORT_FUNCTION DeleteUser(HCL::User *);
 };
 
 #endif //HCL_USER_LINKAGE_H
