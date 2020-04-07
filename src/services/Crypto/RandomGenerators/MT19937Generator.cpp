bbba4ef56a1019b4a6885ef07ddc13d5a2eda4b0
Fantin Bibas <fantin.bibas@epitech.eu>
2020-04-07 12:16:46 +0200

    Add MT19937 random generator

---
+++ src/services/Crypto/RandomGenerators/MT19937Generator.cpp
@@ -0,0 +1,24 @@
+//
+// Created by neodar on 07/04/2020.
+//
+
+#include "MT19937Generator.h"
+
+HCL::Crypto::MT19937Generator::MT19937Generator(const std::string &header, size_t &header_length) :
+  generator_(random_device_),
+  distribution_(0, 255) {
+}
+
+uint8_t HCL::Crypto::MT19937Generator::GenerateRandomByte() {
+  return distribution_(generator_);
+}
+
+std::string HCL::Crypto::MT19937Generator::GenerateRandomByteSequence(size_t sequence_length) {
+  std::string sequence;
+
+  for (size_t i = 0; i < sequence_length; ++i) {
+    sequence += (char) GenerateRandomByte();
+  }
+
+  return sequence;
+}
---
+++ src/services/Crypto/RandomGenerators/MT19937Generator.h
@@ -0,0 +1,26 @@
+//
+// Created by neodar on 07/04/2020.
+//
+
+#ifndef HCL_SRC_SERVICES_CRYPTO_RANDOMGENERATORS_MT19937GENERATOR_H_
+#define HCL_SRC_SERVICES_CRYPTO_RANDOMGENERATORS_MT19937GENERATOR_H_
+
+#include <random>
+#include "../AutoRegisterer.h"
+#include "ARandomGenerator.h"
+
+namespace HCL::Crypto {
+
+class MT19937Generator : AutoRegisterer<ARandomGenerator, MT19937Generator> {
+ public:
+  MT19937Generator(const std::string &header, size_t &header_length);
+  uint8_t GenerateRandomByte() override;
+  std::string GenerateRandomByteSequence(size_t sequence_length) override;
+ private:
+  std::random_device random_device_;
+  std::mt19937 generator_;
+  std::uniform_int_distribution<uint8_t> distribution_;
+};
+}
+
+#endif //HCL_SRC_SERVICES_CRYPTO_RANDOMGENERATORS_MT19937GENERATOR_H_
