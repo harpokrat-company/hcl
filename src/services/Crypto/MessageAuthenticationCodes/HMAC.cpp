7f8aaa17a3cbb6b690b866945d83f54611852670
Fantin Bibas <fantin.bibas@epitech.eu>
2020-04-07 14:10:40 +0200

    Add HMAC implementation

---
+++ src/services/Crypto/MessageAuthenticationCodes/HMAC.cpp
@@ -0,0 +1,27 @@
+//
+// Created by neodar on 07/04/2020.
+//
+
+#include "HMAC.h"
+#include "../CryptoHelper.h"
+
+HCL::Crypto::HMAC::HMAC(const std::string &header, size_t &header_length) {
+  hash_function_ = Factory<AHashFunction>::GetInstanceFromHeader(header, header_length);
+}
+
+std::string HCL::Crypto::HMAC::SignMessage(const std::string &key, const std::string &message) {
+  std::string prepared_key = key;
+  size_t bloc_size = hash_function_->GetBlocSize();
+  std::string outer_key;
+  std::string inner_key;
+
+  if (prepared_key.length() > bloc_size) {
+    prepared_key = hash_function_->HashData(prepared_key);
+  }
+  if (prepared_key.length() < bloc_size) {
+    prepared_key += std::string(bloc_size - prepared_key.length(), (char) 0x00);
+  }
+  outer_key = CryptoHelper::XorStrings(prepared_key, std::string(bloc_size, 0x5c));
+  inner_key = CryptoHelper::XorStrings(prepared_key, std::string(bloc_size, 0x36));
+  return hash_function_->HashData(outer_key + hash_function_->HashData(inner_key + message));
+}
---
+++ src/services/Crypto/MessageAuthenticationCodes/HMAC.h
@@ -0,0 +1,23 @@
+//
+// Created by neodar on 07/04/2020.
+//
+
+#ifndef HCL_SRC_SERVICES_CRYPTO_MESSAGEAUTHENTICATIONCODES_HMAC_H_
+#define HCL_SRC_SERVICES_CRYPTO_MESSAGEAUTHENTICATIONCODES_HMAC_H_
+
+#include "../AutoRegisterer.h"
+#include "AMessageAuthenticationCode.h"
+#include "../HashFunctions/AHashFunction.h"
+
+namespace HCL::Crypto {
+
+class HMAC : public AutoRegisterer<AMessageAuthenticationCode, HMAC> {
+ public:
+  HMAC(const std::string &header, size_t &header_length);
+  std::string SignMessage(const std::string &key, const std::string &message);
+ private:
+  std::unique_ptr<AHashFunction> hash_function_;
+};
+}
+
+#endif //HCL_SRC_SERVICES_CRYPTO_MESSAGEAUTHENTICATIONCODES_HMAC_H_
