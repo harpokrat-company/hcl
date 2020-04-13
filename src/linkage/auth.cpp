5f1738096a939759776c938fd1b6fe6a262d7a3d
Fantin Bibas <fantin.bibas@epitech.eu>
2020-04-13 11:21:52 +0200

    Fix basic auth

--- src/linkage/auth.cpp
+++ src/linkage/auth.cpp
@@ -2,6 +2,8 @@
 // Created by neodar on 12/01/2020.
 //
 
+#include <sstream>
+#include <iomanip>
 #include "auth.h"
 #include "../services/Crypto/Base64.h"
 #include "../services/Crypto/Factory.h"
@@ -12,8 +14,9 @@ std::string *EXPORT_FUNCTION GetBasicAuthString(const char *raw_email, const cha
   auto sha512 = HCL::Crypto::Factory<HCL::Crypto::AHashFunction>::BuildTypedFromName("sha512");
   std::string email(raw_email);
   std::string password(sha512->HashData(raw_password));
+  std::stringstream hex_hash;
 
   // TODO Clean format with signature ?
-  return new std::string("Basic " + HCL::Crypto::Base64::Encode(email + ":" + password));
+  return new std::string("Basic " + HCL::Crypto::Base64::Encode(email + ":" + HCL::Crypto::Base64::Encode(password)));
 }
 }
