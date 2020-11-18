0939edaf2a32efd33dfc344277ab76346d028ce5
Fantin Bibas <fantin@bib.as>
2020-11-18 23:19:11 +0100

    Update hcl.js

--- hcl.js
+++ hcl.js
@@ -1145,9 +1145,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5284912,
+var STACK_BASE = 5284240,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 42032;
+    STACK_MAX = 41360;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
